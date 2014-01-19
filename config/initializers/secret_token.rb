5deb90edbb589a61f8287f281e0c7f067acd76e1
Vasily Vasinov <des.elyon@gmail.com>
2014-01-18 22:07:07 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trains::Application.config.secret_key_base = 'f9c72dab27aec719f4094d09c85d496607789a0aae554cea989a57b9c3908536d3711b68942c47c2e9a0d79df125fdfec474e1e87b8167ce9e599ded6d3c708c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trains::Application.config.session_store :cookie_store, key: '_trains_session'
